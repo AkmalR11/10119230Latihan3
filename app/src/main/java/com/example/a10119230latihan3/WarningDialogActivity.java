6d457e29979973e769af910e751e21f39ca44998
Akmal R <kyukuro88@gmail.com>
2023-04-25 10:41:37 +0700

    .

---
+++ app/src/main/java/com/example/a10119230latihan3/WarningDialogActivity.java
@@ -0,0 +1,17 @@
+package com.example.a10119230latihan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WarningDialogActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warning_dialog);
+    }
+}
+// Nama                 : Akmal Rizqulloh
+// NIM                  : 10119230
+// Kelas                : IF-6
+// Tanggal Pengerjaan   : 19-04-2023
